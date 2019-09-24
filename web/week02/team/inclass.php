55159d90c6f083af99bd4498719486b096b9e1d6
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-09-24 15:49:53 -0600

    in-class activity

---
+++ web/week02/team/inclass.php
@@ -0,0 +1,22 @@
+<doctype html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+	</head>
+
+	<body>
+		<h1>PHP In-Class Team Activity</h1>
+		<?php
+			for ($i = 1; $i <= 10; $i++) {
+				$div = "<div";
+				if ($i % 2 = 0) {
+					$div += " style='color: red'";
+				}
+				$div += ">$i</div>";
+
+				echo $div;
+			}
+		?>
+	</body>
+</html>
