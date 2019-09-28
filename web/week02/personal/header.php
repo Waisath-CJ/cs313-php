7e15dce81c23375ecacbe446fc3c1fd6d7108789
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-09-28 17:13:44 -0600

    uncentered nav

--- web/week02/personal/header.php
+++ web/week02/personal/header.php
@@ -1,6 +1,6 @@
 <?php
 	echo "<h1>CJ Waisath's Website</h1>";
-	echo '<ul class="nav justify-content-center">
+	echo '<ul class="nav">
 				<li class="nav-item">
 					<a class="nav-link" href="#">Home</a>
 				</li>
