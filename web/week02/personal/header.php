86961e07ed1da3c517e5120d03be695a01ae6742
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-09-28 16:58:44 -0600

    fixed issue with file

--- web/week02/personal/header.php
+++ web/week02/personal/header.php
@@ -2,14 +2,14 @@
 	echo "<h1>CJ Waisath's Website</h1>";
 	echo '<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">';
 	echo '<ul class="navbar-nav">
-				<li class="'"nav-item"'">
-					<a class="'"nav-link"'" href="'"#"'">Home</a>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Home</a>
 				</li>
-				<li class="'"nav-item"'">
-					<a class="'"nav-link"'" href="'"#"'">About Me</a>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About Me</a>
 				</li>
-				<li class="'"nav-item"'">
-					<a class="'"nav-link"'" href="'"#"'">Assignments</a>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Assignments</a>
 				</li>
 			</ul>';
 	echo "</nav>";
