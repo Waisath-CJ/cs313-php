1bf3167d4f4db147bacdb01d4e8d2b956fa3a719
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-09-28 16:56:43 -0600

    added the nav bar

--- web/week02/personal/header.php
+++ web/week02/personal/header.php
@@ -1,15 +1,16 @@
 <?php
 	echo "<h1>CJ Waisath's Website</h1>";
-	echo "<nav class='navbar navbar-expand-sm bg-dark navbar-dark fixed-top'>"
-	echo "<ul class='navbar-nav'>
-				<li class='nav-item'>
-					<a class='nav-link' href='#'>Home</a>
+	echo '<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">';
+	echo '<ul class="navbar-nav">
+				<li class="'"nav-item"'">
+					<a class="'"nav-link"'" href="'"#"'">Home</a>
 				</li>
-				<li class='nav-item'>
-					<a class='nav-link' href='#'>About Me</a>
+				<li class="'"nav-item"'">
+					<a class="'"nav-link"'" href="'"#"'">About Me</a>
 				</li>
-				<li class='nav-item'>
-					<a class='nav-link' href='#'>Assignments</a>
+				<li class="'"nav-item"'">
+					<a class="'"nav-link"'" href="'"#"'">Assignments</a>
 				</li>
-			</ul>"
+			</ul>';
+	echo "</nav>";
 ?>
