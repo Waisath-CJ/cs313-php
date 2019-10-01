e27e37b9516e0f377151cf698e331ec96939ab5b
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-01 15:38:20 -0600

    added checkbox handler

--- web/week03/team/handler.php
+++ web/week03/team/handler.php
@@ -10,7 +10,12 @@
 			<p>Your name is: <?php echo $_POST["name"]; ?></p>
 			<p>Your email is: <a href="mailto:<?php echo $_POST["email"]; ?>"><?php echo $_POST["email"]; ?></a></p>	
 			<p>Your major is: <?php echo $_POST["major"]; ?></p>
-			<p>Comments: <?php echo $_POST["comments"]; ?></p>		
+			<p>Comments: <?php echo $_POST["comments"]; ?></p>
+			<p>Continents visited: 
+				<?php foreach ($_POST["continents"] as $continent) {
+					echo $continent . "<br>";
+				} ?>
+			</p>		
 		</div>
 	</body>
 </html>
