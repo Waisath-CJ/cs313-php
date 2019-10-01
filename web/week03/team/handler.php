71f56257d4bb8435a900b7154977e64dc0b0b1b7
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-01 15:10:07 -0600

    added title and content

---
+++ web/week03/team/handler.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Team Activity - Week 03</title>
+		<meta charset="utf-8">
+	</head>
+	
+	<body>
+		<div id="wrapper">
+			<?php
+				
+			?>			
+		</div>
+	</body>
+</html>
