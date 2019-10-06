6a258d28d77e3cbbd6a6aa151eb531658dc1b7b3
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-05 18:51:46 -0600

    added navigation code

---
+++ web/week03/personal/nav.php
@@ -0,0 +1,15 @@
+<?php
+	echo "<header>";
+	echo '<ul class="nav nav-pills nav-justified">
+				<li class="nav-item">
+					<a class="nav-link" href="index.php">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="about.php">About Me</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="assignments.php">Assignments</a>
+				</li>
+			</ul>';
+	echo "</header>";
+?>
