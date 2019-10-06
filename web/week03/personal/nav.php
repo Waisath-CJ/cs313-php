60d7b4c33454ea0e872d9575b91a7cace1826ddd
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-05 18:54:26 -0600

    updated nav locations to current locations

--- web/week03/personal/nav.php
+++ web/week03/personal/nav.php
@@ -2,13 +2,13 @@
 	echo "<header>";
 	echo '<ul class="nav nav-pills nav-justified">
 				<li class="nav-item">
-					<a class="nav-link" href="index.php">Home</a>
+					<a class="nav-link" href="browse.php">Browse Items</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="about.php">About Me</a>
+					<a class="nav-link" href="viewCart.php">View Cart</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="assignments.php">Assignments</a>
+					<a class="nav-link" href="checkout.php">Checkout</a>
 				</li>
 			</ul>';
 	echo "</header>";
