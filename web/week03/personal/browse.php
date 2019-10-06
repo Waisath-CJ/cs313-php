ff2c6a5d40427323984ded051530fbc9a3c77012
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-05 19:07:52 -0600

    changed nav location

--- web/week03/personal/browse.php
+++ web/week03/personal/browse.php
@@ -18,7 +18,20 @@
 	
 	<body>
 		<div id="wrapper">
-			<?php include "nav.php"; ?>
+			<header>
+				<h1 class="display-2 text-center">The Video Game Emporium</h1>
+				<ul class="nav nav-pills nav-justified">
+					<li class="nav-item">
+						<a class="nav-link active" href="browse.php">Browse Items</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="viewCart.php">View Cart</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="checkout.php">Checkout</a>
+					</li>
+				</ul>
+			</header>
 
 			
 		</div>
