a63db7a8b173486f96204916a4f014a08af353ea
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-24 16:00:56 -0600

    update

--- dbFiles/project1db.sql
+++ dbFiles/project1db.sql
@@ -12,16 +12,19 @@ CREATE TABLE Customers (
 
 CREATE TABLE Products (
 	product_id SERIAL PRIMARY KEY,
-	quantity int NOT NULL,
-	product_type varchar NOT NULL
+	product varchar NOT NULL,
+	quantity int NOT NULL
 );
 
 CREATE TABLE Orders (
 	order_id SERIAL PRIMARY KEY,
 	order_number SERIAL,
-	customer_id int REFERENCES Customers(id)
+	customer_id int REFERENCES Customers(id),
+	product_id int REFERENCES Products(id)
 );
 
 INSERT INTO Customers(firstName, lastName, street, city, state, zip) VALUES ('CJ', 'Waisath', '3903 E Huber St', 'Mesa', 'AZ', '85205');
 INSERT INTO Customers(firstName, lastName, street, city, state, zip) VALUES ('Josh', 'Fast', '649 S 2nd W Apt 3406', 'Rexburg', 'ID', '83440');
 INSERT INTO Customers(firstName, lastName, street, city, state, zip) VALUES ('Anne', 'Bonham', '310 E 10600 S Apt 11', 'Sandy', 'UT', '84070');
+
+INSERT INTO Products(product, quantity) VALUES ('Sugar Cookies', '4'), ('Chocolate Chip Cookies', '12');
