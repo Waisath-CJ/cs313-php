2cdca756213f5c2be921696da0a337e3737df654
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-25 10:31:19 -0600

    t

--- web/week05-07/personal/delights.php
+++ web/week05-07/personal/delights.php
@@ -49,8 +49,6 @@
                 }
                 echo '</div>';
             ?>
-            <p class="h3">Looking to buy some?</p>
-            <button type="button" class="btn btn-outline-dark btn-block">Schedule a Consultation</button>
         </div>
     </body>
 </html>
