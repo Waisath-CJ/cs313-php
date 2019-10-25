b536ffd4c97a8c780fc7dc1c62fc477903f1b05c
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-25 10:21:07 -0600

    button styling

--- web/week05-07/personal/delights.php
+++ web/week05-07/personal/delights.php
@@ -50,7 +50,7 @@
                 echo '</div>';
             ?>
             <p class="h3 text-center">Looking to buy some?</p>
-            <button type="button" class="btn btn-outline-dark btn-lg btn-block">Schedule a Consultation</button>
+            <button type="button" class="btn btn-outline-dark btn-block">Schedule a Consultation</button>
         </div>
     </body>
 </html>
