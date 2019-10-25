c15a82b41c95e7c96e9859ebb287855c00150493
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-25 11:18:46 -0600

    updated database

--- dbFiles/project1db.sql
+++ dbFiles/project1db.sql
@@ -1,14 +1,4 @@
-DROP TABLE Consultations, Customers, Flavors, BakedGoods;
-
-CREATE TABLE Customers (
-	id SERIAL PRIMARY KEY,
-	firstName varchar NOT NULL,
-	lastName varchar NOT NULL,
-	email varchar NOT NULL,
-	phone bigint NOT NULL,
-	username varchar UNIQUE,
-	password varchar 
-);
+DROP TABLE Consultations, Flavors, BakedGoods;
 
 CREATE TABLE BakedGoods (
 	id SERIAL PRIMARY KEY,
@@ -23,21 +13,21 @@ CREATE TABLE Flavors (
 
 CREATE TABLE Consultations (
 	id SERIAL PRIMARY KEY,
-	customer_id int REFERENCES Customers(id),
+	firstName varchar NOT NULL,
+	lastName varchar NOT NULL,
+	email varchar NOT NULL,
+	phone bigint NOT NULL,
 	consult_type int REFERENCES BakedGoods(id),
 	date DATE NOT NULL,
 	time TEXT NOT NULL
 );
 
-INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('CJ', 'Waisath', 'soccerboycj.97@hotmail.com', '4806332188');
-INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Josh', 'Fast', 'jfast@gmail.com', '8012223322');
-INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Anne', 'Bonham', 'annebonham@gmail.com', '8013456789');
 INSERT INTO BakedGoods(name) VALUES ('Cookies'), ('Brownies'), ('Cupcakes'), ('Cakes'), ('Pies'); 
 INSERT INTO Flavors(bakedGood_id, flavor) VALUES ('1', 'Sugar'), ('1', 'Chocolate Chip'), ('1', 'Lemon'), ('1', 'Peanut Butter Ganache'), ('1', 'Chocolate Ganache'), ('1', 'White Chocolate Macadamia');
 INSERT INTO Flavors(bakedGood_id, flavor) VALUES ('2', 'Chocolate'), ('2', 'Mint'), ('2', 'Smores');
 INSERT INTO Flavors(bakedGood_id, flavor) VALUES ('3', 'Vanilla'), ('3', 'Chocolate'), ('3', 'Red Velvet'), ('3', 'Strawberry Shortcake'), ('3', 'Lemon'), ('3', 'Coconut Creme'), ('3', 'Boston Creme'), ('3', 'Oreo');
 INSERT INTO Flavors(bakedGood_id, flavor) VALUES ('4', 'Vanilla'), ('4', 'Chocolate'), ('4', 'Marble'), ('4', 'Pineapple'), ('4', 'Strawberry'), ('4', 'White Chocolate'), ('4', 'Red Velvet');
 INSERT INTO Flavors(bakedGood_id, flavor) VALUES ('5', 'Apple'), ('5', 'Pumpkin'), ('5', 'Cherry'), ('5', 'Raspberry');
-INSERT INTO Consultations(customer_id, consult_type, date, time) VALUES ('1', '3', '2019-10-30', '11:00:00AM');
+INSERT INTO Consultations(firstName, lastName, email, phone, consult_type, date, time) VALUES ('CJ', 'Waisath', 'soccerboycj.97@hotmail.com', '4806332188', '3', '2019-10-30', '11:00:00AM');
 
 \i project1query.sql
