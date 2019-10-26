b3440199ccc4b4f1a551bdf522ee0372e108dc79
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-26 11:27:38 -0600

    updated confirmation page

---
+++ web/week05-07/personal/confirmation.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Whit's Baked Delights</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <?php require('header.php'); ?>
+
+            <h1 class="display-3 text-center">Thank You for your Consultation request!</h1>
+            <p class="text-muted text-center">We will reach out to you within the next 48 hours with more information regarding your consultation.</p>
+        </div>
+    </body>
+</html>
