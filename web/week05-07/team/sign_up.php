f0a162628a161c6ed2d1d5359b929c1202fe940b
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-29 16:56:55 -0600

    t

--- web/week05-07/team/sign_up.php
+++ web/week05-07/team/sign_up.php
@@ -1,17 +1,4 @@
-<?php 
-	session_start();
-​
-	if (isset($_SESSION['userId']))
-	{
-		header("Location: welcome.php");
-		die();
-	}
-​
-	$message = $_GET['message'];
-?>
-​
 <!DOCTYPE html>
-​
 <html>
   <head>
     <meta charset="UTF-8">
