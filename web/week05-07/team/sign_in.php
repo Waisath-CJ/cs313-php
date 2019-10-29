b8b42e837c9f1e52c62c3effebe4cb18cf4fdbb6
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-29 17:02:02 -0600

    t

--- web/week05-07/team/sign_in.php
+++ web/week05-07/team/sign_in.php
@@ -1,44 +1,31 @@
-<?php 
-	session_start();
-​
-	if (isset($_SESSION['userId']))
-	{
-		header("Location: welcome.php");
-		die();
-	}
-​
-	$message = $_GET['message'];
-?>
-​
-<!DOCTYPE html>
-​
+<!DOCTYPE html>    ​
 <html>
-  <head>
-    <meta charset="UTF-8">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <title>Sign-In</title>
-  </head>
-  <body>
-	<h1>Sign-In</h1>
-	<?php 
-		if (isset($message)){
-			echo "<div>$message</div>";
-		}
-	?>
-	<form action="login.php" method="post">
-	  <div class="form-group">
-		<label for="username">Username:</label>
-		<input type="text" class="form-control" id="username" name="username">
-	  </div>
-	  <div class="form-group">
-		<label for="pwd">Password:</label>
-		<input type="password" class="form-control" id="pwd" name="pwd">
-	  </div>
-	  <button type="submit" class="btn btn-default">Submit</button>
-	</form>
-	<br/><br/>
-	<div>Don't have an account? <a href="sign-up.php">Sign Up</a></div>
-  </body>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <title>Sign-In</title>
+    </head>
+    <body>
+        <h1>Sign-In</h1>
+        <?php 
+            if (isset($message)){
+                echo "<div>$message</div>";
+            }
+        ?>
+        <form action="login.php" method="post">
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="text" class="form-control" id="username" name="username">
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd" name="pwd">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        <br/><br/>
+        <div>Don't have an account? <a href="sign-up.php">Sign Up</a></div>
+    </body>
 </html>
--- web/week05-07/team/welcome.php
+++ web/week05-07/team/welcome.php
@@ -1,21 +1,14 @@
-<?php 
-	session_start();
-​
-	if (!isset($_SESSION['userId']))
-	{
-		header("Location: sign-in.php");
-		die();
-	}
-?>
-​
 <!DOCTYPE html>
-​
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-  </head>
-  <body>
-	<h1>Welcome!</h1>
-  </body>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <title>Welcome</title>
+    </head>
+    
+    <body>
+        <h1>Welcome!</h1>
+    </body>
 </html>
