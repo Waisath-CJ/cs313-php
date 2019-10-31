2dc3d98ad1bf17c035fa80d3484d45ef6dba4352
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-10-31 15:41:51 -0600

    added dynamic nav stuff

--- web/week05-07/personal/header.php
+++ web/week05-07/personal/header.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+
     echo '<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #FF98E3;">
     <a class="navbar-brand" href="index.php">Whit\'s Baked Delights</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,11 +9,18 @@
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-item nav-link" href="index.php">Home</a>
-        <a class="nav-item nav-link" href="delights.php">Baked Delights</a>
-        <a class="nav-item nav-link" href="consult.php">Schedule a Consultation</a>
-        <a class="nav-item nav-link" href="about.php">About Us</a>
-        <a class="nav-item nav-link" href="contact.php">Contact Me</a>
-      </div>
-    </div>
-  </nav>';
+        <a class="nav-item nav-link" href="delights.php">Baked Delights</a>';
+
+        if (isset($_SESSION['userId'])) {
+          echo '<a class="nav-item nav-link" href="consult.php">Schedule a Consultation</a>';
+        }
+
+        echo '<a class="nav-item nav-link" href="about.php">About</a>'
+
+        if (!isset($_SESSION['userId'])) {
+          echo '<a class="nav-item nav-link" href="logout.php">Logout</a>';
+        } else {
+          echo '<a class="nav-item nav-link" href="login.php">Login</a>';
+        }
+        echo '</div></div></nav>';
 ?>
