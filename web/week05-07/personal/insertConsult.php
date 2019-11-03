55f82bcd2a393dd701dba6bcf5a7b4190fc07199
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-11-02 21:41:39 -0600

    added error handling and no repeats for date and time

--- web/week05-07/personal/insertConsult.php
+++ web/week05-07/personal/insertConsult.php
@@ -3,27 +3,61 @@
     
     require("dbConnect.php");
     $db = get_db();
+
+    function errorMessage($type) {
+        $message;
+
+        if ($type == 1) {
+            $message = "All fields required!";
+        } elseif ($type == 2) {
+            $message = "Database error! Please try again later.";
+        } elseif ($type == 3) {
+            $message = "Date and Time are already taken!";
+        }
+
+        header("Location: consult.php?message=$message&type=$type");
+        die();
+    }
     
     $consultType = (int) $_POST['consultType'];
     $date = $_POST['date'];
     $time = $_POST['time'];
 
-    try
-    {
-        $query = 'INSERT INTO Consultations(customer_id, consult_type, date, time) VALUES(:customer_id, :consultType, :date, :time)';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':customer_id', $_SESSION['userId'], PDO::PARAM_INT);
-        $statement->bindValue(':consultType', $consultType, PDO::PARAM_INT);
-        $statement->bindValue(':date', $date);
-        $statement->bindValue(':time', $time);
-        $statement->execute();
-    }
-    catch (Exception $ex)
-    {
-        echo "Error with DB. Details: $ex";
+    if (!empty($consultType) && !empty($date) && !empty($time)) {
+        try {
+            $query = 'SELECT * FROM Consultations';
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                if ($date == $row['date'] && $time == $row['time']) {
+                    errorMessage(3);
+                }
+            }
+        }   
+        catch (Exception $ex) {
+
+        }
+        
+        try
+        {
+            $query = 'INSERT INTO Consultations(customer_id, consult_type, date, time) VALUES(:customer_id, :consultType, :date, :time)';
+            $statement = $db->prepare($query);
+            $statement->bindValue(':customer_id', $_SESSION['userId'], PDO::PARAM_INT);
+            $statement->bindValue(':consultType', $consultType, PDO::PARAM_INT);
+            $statement->bindValue(':date', $date);
+            $statement->bindValue(':time', $time);
+            $statement->execute();
+        }
+        catch (Exception $ex)
+        {
+            echo "Error with DB. Details: $ex";
+            die();
+        }
+    
+        header("Location: confirmation.php");
         die();
+    } else {
+        errorMessage(1);
     }
 
-    header("Location: confirmation.php");
-    die();
 ?>
