6306405ecc7d921b2dacb67c40c074550ab5bd89
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-11-02 22:47:16 -0600

    updated footer styling

--- web/week05-07/personal/footer.php
+++ web/week05-07/personal/footer.php
@@ -1,5 +1,5 @@
 <?php
     echo '<br>';
     echo '<footer style="background-color: #FF98E3;">';
-    echo '<p> This website was made for CS 313 at BYU-Idaho.</p>';
+    echo '<p class="text-center"> This website was made for CS 313 at BYU-Idaho.</p>';
 ?>
