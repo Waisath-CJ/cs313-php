6ee740922b92c32a24cd43520223734758839eb8
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-11-02 19:53:47 -0600

    fixed db code

--- web/week05-07/personal/loginHandler.php
+++ web/week05-07/personal/loginHandler.php
@@ -24,38 +24,24 @@
     //echo $username."<br>".$pwd;
 
     if (!empty($username) && !empty($pwd)) {
-        $userId;
-        $dbPWD;
         try {
-            $query = 'SELECT id, password FROM Customers WHERE username = :username';
+            $query = 'SELECT id, username, password FROM Customers WHERE username = :username';
             $stmt = $db->prepare($query);
             $stmt->bindValue(':username', $username, PDO::PARAM_STR);
             $stmt->execute();
+
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($row) {
-                errorMessage(1);
+            
+            if ($row && password_verify($pwd, $row['password'])) {
+                $_SESSION['userId'] = $userId;
+                header("Location: index.php");
+                die();
             } else {
-                $userId = $row['id'];
-                $dbPWD = $row['password'];
+                errorMessage(1);
             }
         }
         catch (Exception $ex) {
             errorMessage(2);
-        }
-
-        if (password_verify($pwd, $dbPWD)) {
-            /* try {
-                $query = 
-            }
-            catch (Exception $ex) {
-                errorMessage(2);
-            } */
-            
-            $_SESSION['userId'] = $userId;
-            header("Location: index.php");
-            die();
-        } else {
-            errorMessage(1);
         }
     } else {
         errorMessage(3);
