34863fa12505f9ed64b1303d7d7488a76c8233e1
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-11-02 18:41:24 -0600

    testing form

--- web/week05-07/personal/createAccount.php
+++ web/week05-07/personal/createAccount.php
@@ -1,4 +1,13 @@
 <?php
     require("dbConnect.php");
     $db = get_db();
+
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+    $cpwd = $_POST['cpwd'];
+
+    echo $firstName."<br>".$lastName."<br>".$email."<br>".$username."<br>".$pwd."<br>".$cpwd;
 ?>
