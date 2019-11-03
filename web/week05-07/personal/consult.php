006e270c8c985f237a2fbabb01d1a3934d83a843
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-11-02 20:22:25 -0600

    error checking

--- web/week05-07/personal/consult.php
+++ web/week05-07/personal/consult.php
@@ -43,13 +43,14 @@
             </nav>
             <h1 class="display-3 text-center">Get your own Baked Delights!</h1>
             <p class="text-muted text-center">Schedule your consultation today so you can get your own delicions Baked Delights!</p>
-            <img src="pics/bakedGoods.jpg" class="rounded mx-auto d-block" alt="Assorted Baked Goods">
+            <img src="pics/bakedGoods.jpg" class="rounded mx-auto d-block" alt="Assorted Baked Goods"><br>
             <?php 
                 $query = 'SELECT firstName, lastName FROM Customers WHERE id = :id';
                 $stmt = $db->prepare($query);
                 $stmt->bindValue(':id', $_SESSION['userId']);
                 $stmt->execute();
                 $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                echo $_SESSION['userId']."<br>";
                 echo "<b>Requesting a consultation for: ".$row['firstName']." ".$row['lastName']."</b>";
             ?>
             <form action="insertConsult.php" method="POST">
