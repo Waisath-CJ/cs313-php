75e236c5e7ecb9071c0ffc1ac2dca7fa3e97abce
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2019-11-02 19:30:38 -0600

    testing login form

--- web/week05-07/personal/loginHandler.php
+++ web/week05-07/personal/loginHandler.php
@@ -1,3 +1,23 @@
 <?php
+    session_start();
+    require('dbConnect.php');
+    $db = get_db();
 
+    function errorMessage($type) {
+        $message;
+
+        if ($type == 1) {
+            $message = "Incorrect username or password!";
+        } elseif ($type == 2) {
+            $message = "Database error! Please try again later.";
+        }
+
+        header("Location: login.php?message=$message&type=$type");
+        die();
+    }
+
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+
+    echo $username."<br>".$pwd;
 ?>
