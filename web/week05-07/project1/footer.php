1ae5775f3ecb61c472a919633ddaec0a70ea42e2
Waisath-CJ <curt.waisath.jr@outlook.com>
2020-06-03 14:01:26 -0700

    updated footer message

--- web/week05-07/project1/footer.php
+++ web/week05-07/project1/footer.php
@@ -1,5 +1,6 @@
 <?php
     echo '<br>';
     echo '<footer style="background-color: #FF98E3;">';
-    echo '<p class="text-center"> This website was made for CS 313 at BYU-Idaho.</p>';
+    echo '<p class="text-center">This website is a basic concept for a baker\'s website. This is not a real site.</p>';
+    echo '</footer>';
 ?>
